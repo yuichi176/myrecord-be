c21414848529ddc0e5532a08aaf6d7b09334ded8
Yuichi Sugiyama <ozwald176@gmail.com>
2023-02-06 23:56:26 +0900

    first commit

---
+++ src/main/kotlin/com/example/myrecordbe/MyrecordBeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.myrecordbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyrecordBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyrecordBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/myrecordbe/MyrecordBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.myrecordbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyrecordBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
